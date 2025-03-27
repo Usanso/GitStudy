88de447468168dabc7f4766e4eddb2602c9a7689
Usanso <save1086@naver.com>
2025-03-28 00:55:50 +0900

    Array_lv5
    배열 레벨5 문제까지 완료

---
+++ Array_3/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Array_3
+{
+    internal class Program
+    {
+        // Level 4: 중복 제거 후 정렬하기
+        // 정수 배열이 주어졌을 때, 중복된 원소를 제거하고 오름차순으로 정렬하는 프로그램을 작성하시오.
+        // int[] numbers = { 4, 2, 7, 2, 1, 4, 5, 7 };
+        // 정렬된 배열: 1 2 4 5 7
+
+        // 정렬기능 Array.Sort() 메서드
+        static void Main(string[] args)
+        {
+            int[] numbers = { 4, 2, 7, 2, 1, 4, 5, 7 };
+
+            int[] numArray = numbers.Distinct().ToArray(); // 중복값 제거
+            Array.Sort(numArray); // 순서 오름차순 정렬
+
+            foreach (int i in numArray)
+            {
+                Console.Write(i);
+            }
+
+        }
+    }
+}
---
+++ Array_4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Array_4
+{
+    internal class Program
+    {
+        // Level 5: 두 배열의 교집합 구하기
+        // 두 개의 정수 배열이 주어졌을 때, 두 배열의 공통된 원소(교집합)를 찾는 프로그램을 작성하시오.
+        // 입력 예시: int[] arr1 = { 1, 2, 3, 4, 5 }; int[] arr2 = { 3, 4, 5, 6, 7 };
+        // 출력 예시: 교집합 3 4 5
+
+        static void Main(string[] args)
+        {
+            int[] arr1 = { 1, 2, 3, 4, 5 };
+            int[] arr2 = { 3, 4, 5, 6, 7 };
+            Console.Write("교집합 : ");
+
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                for (int j = 0; j < arr2.Length; j++)
+                {
+                    if (arr1 [i] == arr2[j])
+                    {
+                        Console.Write(arr1[i]);
+                    } 
+                }
+            }
+
+        }
+    }
+}
